2720a4eb7a1f8a4047cb6c884e08ee06106e59f1
Michal Geci <michal.geci.1995@gmail.com>
2020-10-05 23:39:52 +0200

    Added language support

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Container } from 'shards-react'
+
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css"
+
+import './i18n'
+import Navigation from './Navigation';
+import { HashRouter, Route } from 'react-router-dom';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter basename="/">
+      <Container>
+        <br/>
+        <Navigation/>
+        <Route exact path="/" component={() => <div>Ahoj</div>}/>
+        <Route exact path="/projects" component={() => <div>Projects</div>}/>
+      </Container>
+    </HashRouter>
   );
 }
 
---
+++ src/Navigation.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Button, ButtonGroup, Nav, NavItem, NavLink } from 'shards-react'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css"
+
+import { useTranslation } from 'react-i18next'
+import { useLocation } from 'react-router-dom'
+
+function Navigation() {
+
+  const { t, i18n } = useTranslation()
+  const changeLanguage = (langCode) => {
+    i18n.changeLanguage(langCode)
+  }
+  const location = useLocation()
+
+  return (
+    <Nav tabs>
+
+      <NavItem>
+        <NavLink active={location.pathname === "/"} href="#/">
+          {t('about_me')}
+        </NavLink>
+      </NavItem>
+
+      <NavItem>
+        <NavLink active={location.pathname === "/projects"} href="#/projects">
+          {t('my_projects')}
+        </NavLink>
+      </NavItem>
+
+
+
+      <li style={{ marginLeft: 'auto' }}>
+        <ButtonGroup>
+          <Button outline={i18n.language !== "sk"} onClick={() => { changeLanguage("sk") }}>SK</Button>
+          <Button outline={i18n.language !== "en"} onClick={() => { changeLanguage("en") }}>EN</Button>
+        </ButtonGroup>
+      </li>
+    </Nav>
+  )
+}
+
+export default Navigation
