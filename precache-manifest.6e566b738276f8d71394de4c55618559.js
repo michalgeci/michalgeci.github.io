fc19b2c2ceadb21e102f4e5830bc75be2899b643
Michal Geci <michal.geci.1995@gmail.com>
2020-10-14 02:27:50 +0200

    Updates

---
+++ precache-manifest.6e566b738276f8d71394de4c55618559.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d17a3aa50c5348627aa58abffe3c634",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3c413c7615271b77405",
+    "url": "/static/css/2.78afd188.chunk.css"
+  },
+  {
+    "revision": "2fdca4e34853a2dc106d",
+    "url": "/static/css/main.6d46b6ac.chunk.css"
+  },
+  {
+    "revision": "c3c413c7615271b77405",
+    "url": "/static/js/2.f4154878.chunk.js"
+  },
+  {
+    "revision": "a08e7a1a59bbf100bf2ed39a8d12e9ea",
+    "url": "/static/js/2.f4154878.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fdca4e34853a2dc106d",
+    "url": "/static/js/main.e3fedc8e.chunk.js"
+  },
+  {
+    "revision": "5e085f63831bab725976",
+    "url": "/static/js/runtime-main.285894c7.js"
+  },
+  {
+    "revision": "c4d7543ff24a3dff35c9f5321be3aea9",
+    "url": "/static/media/404.c4d7543f.png"
+  },
+  {
+    "revision": "7f4bef28e328384e8a9198337acf541f",
+    "url": "/static/media/bowtie.7f4bef28.jpg"
+  },
+  {
+    "revision": "e0d787f092cb9be72498adbe5cf26dbb",
+    "url": "/static/media/covid.e0d787f0.png"
+  },
+  {
+    "revision": "2e0e226ec86ffdf7cb9fc290a182a685",
+    "url": "/static/media/covid_2x.2e0e226e.png"
+  },
+  {
+    "revision": "3752d430b97bfb9e890d90a7c79f5a8e",
+    "url": "/static/media/education.3752d430.svg"
+  },
+  {
+    "revision": "b83f4cfaf6b4f77b325a7c7434eb0db4",
+    "url": "/static/media/figgis.b83f4cfa.png"
+  },
+  {
+    "revision": "8cc060bb2e9501bb279b5ea52da8ff83",
+    "url": "/static/media/goodTime.8cc060bb.ttf"
+  },
+  {
+    "revision": "12d2cd05631c1ca8871461be972a6cb7",
+    "url": "/static/media/krieg.12d2cd05.png"
+  },
+  {
+    "revision": "487b2696903ce416a7f89b68934d432d",
+    "url": "/static/media/notes.487b2696.png"
+  },
+  {
+    "revision": "e46e4bba1d45a27c1e266e636a855f1c",
+    "url": "/static/media/notes_2x.e46e4bba.png"
+  },
+  {
+    "revision": "87086a84686c630213cd6196da8c1c36",
+    "url": "/static/media/planet.87086a84.png"
+  },
+  {
+    "revision": "8c45ca5a0c8f7825cb9f153eb3201825",
+    "url": "/static/media/proofs_2x.8c45ca5a.png"
+  },
+  {
+    "revision": "25ae6c6cfba555bd0d7f1a921ca6037e",
+    "url": "/static/media/rick_morty.25ae6c6c.png"
+  },
+  {
+    "revision": "38e9f15aa1f8221b8b7bee6f81b909dc",
+    "url": "/static/media/rick_morty_2x.38e9f15a.png"
+  },
+  {
+    "revision": "e24d57b18956c4a4c32ea6cf1813c4c4",
+    "url": "/static/media/secretHitler.e24d57b1.png"
+  },
+  {
+    "revision": "7378d7967bc9e4be407519f926ebb00b",
+    "url": "/static/media/secretHitler_2x.7378d796.png"
+  },
+  {
+    "revision": "450ffaa5e9f10b0730d4b8992da56d61",
+    "url": "/static/media/sith.450ffaa5.jpg"
+  },
+  {
+    "revision": "cd911c70e573e231e9eaa1100dcf3efe",
+    "url": "/static/media/thats_all.cd911c70.gif"
+  },
+  {
+    "revision": "e49bb4008f132f707ce10fa3a822767a",
+    "url": "/static/media/thug_life.e49bb400.jpg"
+  },
+  {
+    "revision": "c5f58f1a24e678986adc5bcd992c4781",
+    "url": "/static/media/work.c5f58f1a.svg"
+  },
+  {
+    "revision": "22897ca8b38e2d78cc055cb5ed228ffa",
+    "url": "/static/media/wrench.22897ca8.svg"
+  }
+]);
